223f24081773fe4ba03bac9efa4434d1501d11f3
alfredogelk <65130590+alfredogelk@users.noreply.github.com>
2021-07-04 15:48:49 -0300

    Add files via upload

---
+++ filmes.js
@@ -0,0 +1,143 @@
+<h1 mat-dialog-title>{{titulo}} </h1>
+import {Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+import {Observable } from 'rxjs';
+import {Filme } from '../shared/models/Filme';
+
+const url = 'http://localhost:3000/filmes/';
+
+
+ filmes: Filme();
+
+
+ onscroll(); void {
+this:FilmelistarFilmes()
+
+
+ }
+ 
+abrir(id.FilmelistarFilmes ); 
+
+path: ':id';
+
+component: CadastroFilmesComponent;
+{
+path: 'cadastro';
+component: CadastroFilmesComponent;
+pathMatch: 'full'}
+
+
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, OnInit} from '@angular/router';
+
+
+
+editar(); void 
+
+this.router.navigateByUrl('/filmes/cadastro/'  +  this.id);
+
+ get ()
+{
+return this.cadastro.controls;
+
+}
+
+
+def (x, y)
+x/y.to_f
+encodeURI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
